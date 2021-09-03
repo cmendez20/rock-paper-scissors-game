186d8e6bd4e0d6ead7aa62e42313974c06c55f34
Chris-Windows-Desktop <cmendez20@utexas.edu>
2021-09-03 00:02:45 -0500

    Updated choices DOM to use event bubbling

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+// Selecting our elements from the DOM
+const choices = document.querySelector('.choices');
+const resultsDiv = document.querySelector('.results');
+const displayComputerWins = document.querySelector('.computerWins');
+const displayPlayerWins = document.querySelector('.playerWins');
+const restartBtn = document.querySelector('.restart');
+
+// Global Variables
+let playerWins = 0;
+let computerWins = 0;
+let draw = 0;
+let computerSelection, playerSelection;
+
 const computerPlay = function () {
   // Generates a random number from 0 - 2
   let randChoice = Math.floor(Math.random() * 3);
 
   // Generates Computer Play from an array of possible choices using randNum as an index
   const computerChoices = ['rock', 'paper', 'scissors'];
-  console.log(computerChoices[randChoice]);
+  // console.log(computerChoices[randChoice]);
   return computerChoices[randChoice];
 };
 
 const playRound = function (playerSelection, computerSelection) {
-  console.log(playerSelection);
-  console.log(computerSelection);
+  console.log(`Player's Choice: ${playerSelection}`);
+  console.log(`Computer's Choice: ${computerSelection}`);
   const playerSelectionLowerCase = playerSelection.toLowerCase();
 
   // console.log(playerSelectionLowerCase);
@@ -30,73 +43,6 @@ const playRound = function (playerSelection, computerSelection) {
   }
 };
 
-const choices = document.querySelectorAll('.choices');
-const resultsDiv = document.querySelector('.results');
-const displayComputerWins = document.querySelector('.computerWins');
-const displayPlayerWins = document.querySelector('.playerWins');
-const restartBtn = document.querySelector('.restart');
-let playerWins = 0;
-let computerWins = 0;
-let draw = 0;
-let computerSelection;
-let playerSelection;
-
-const game = function () {
-  // computerSelection = computerPlay();
-  // console.log(computerSelection);
-
-  choices.forEach(button =>
-    button.addEventListener('click', function (e) {
-      computerSelection = computerPlay();
-      resultsDiv.textContent = '';
-      playerSelection = this.textContent;
-      // console.log(this.textContent);
-      console.log(playerSelection);
-      console.log(computerSelection);
-      const playerSelectionLowerCase = playerSelection.toLowerCase();
-
-      // console.log(playerSelectionLowerCase);
-      // console.log(computerSelection);
-      if (
-        (playerSelectionLowerCase === 'rock' &&
-          computerSelection === 'paper') ||
-        (playerSelectionLowerCase === 'scissors' &&
-          computerSelection === 'rock') ||
-        (playerSelectionLowerCase === 'paper' &&
-          computerSelection === 'scissors')
-      ) {
-        computerWins++;
-        computerSelection = computerSelection.replace(
-          computerSelection[0],
-          computerSelection[0].toUpperCase()
-        );
-        resultsDiv.textContent += `You lose! ${computerSelection} beats ${playerSelection}.`;
-        displayComputerWins.textContent = `Computer's Score: ${computerWins}`;
-        checkWinner(computerWins, playerWins);
-      } else if (
-        (computerSelection === 'rock' &&
-          playerSelectionLowerCase === 'paper') ||
-        (computerSelection === 'scissors' &&
-          playerSelectionLowerCase === 'rock') ||
-        (computerSelection === 'paper' &&
-          playerSelectionLowerCase === 'scissors')
-      ) {
-        playerWins++;
-        resultsDiv.textContent += `You win! ${playerSelection} beats ${computerSelection}.`;
-        displayPlayerWins.textContent = `Player's Score: ${playerWins}`;
-        checkWinner(computerWins, playerWins);
-      } else {
-        draw++;
-        resultsDiv.textContent += "It's a tie!";
-      }
-      restartBtn.addEventListener('click', restartGame);
-    })
-  );
-
-  // console.log(playerWins, computerWins, draw);
-  // console.log(checkWinner(playerWins, computerWins));
-};
-
 const checkWinner = (computerScore, playerScore) => {
   if (computerScore > playerScore && computerScore === 5) {
     resultsDiv.textContent = 'Game over! The computer won :(';
@@ -114,6 +60,46 @@ const restartGame = function () {
   playerWins = 0;
   computerWins = 0;
   restartBtn.style.opacity = 0;
+};
+
+const game = function () {
+  choices.addEventListener('click', function (e) {
+    computerSelection = computerPlay();
+    playerSelection = e.target.textContent;
+    console.log(`Player's choice: ${playerSelection}`);
+    console.log(`Computer's choice: ${computerSelection}`);
+    const playerSelectionLowerCase = playerSelection.toLowerCase();
+
+    if (
+      (playerSelectionLowerCase === 'rock' && computerSelection === 'paper') ||
+      (playerSelectionLowerCase === 'scissors' &&
+        computerSelection === 'rock') ||
+      (playerSelectionLowerCase === 'paper' && computerSelection === 'scissors')
+    ) {
+      computerWins++;
+      computerSelection = computerSelection.replace(
+        computerSelection[0],
+        computerSelection[0].toUpperCase()
+      );
+      resultsDiv.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+      displayComputerWins.textContent = `Computer's Score: ${computerWins}`;
+      checkWinner(computerWins, playerWins);
+    } else if (
+      (computerSelection === 'rock' && playerSelectionLowerCase === 'paper') ||
+      (computerSelection === 'scissors' &&
+        playerSelectionLowerCase === 'rock') ||
+      (computerSelection === 'paper' && playerSelectionLowerCase === 'scissors')
+    ) {
+      playerWins++;
+      resultsDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+      displayPlayerWins.textContent = `Player's Score: ${playerWins}`;
+      checkWinner(computerWins, playerWins);
+    } else {
+      draw++;
+      resultsDiv.textContent = "It's a tie!";
+    }
+  });
+  restartBtn.addEventListener('click', restartGame);
 };
 
 // Play 5 rounds of rock, paper, scissors
